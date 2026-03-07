bc93ba61d7367fef4f11a9299cc409aed6ec8115
ghstll <129940439+ghstll@users.noreply.github.com>
2026-03-07 02:15:13 -0600

    query product test

--- tests/client/test_product_client.py
+++ tests/client/test_product_client.py
@@ -15,7 +15,7 @@ import pytest
 import jub.config as CX
 from nanoid import generate as nanoid
 from jub.client import JubClient
-from jub.dto import Catalog, KindEnum, Level, LevelCatalog, Observatory, Product
+from jub.dto import Catalog, KindEnum, Level, LevelCatalog, Observatory, Product, ProductFilter
 
 @pytest.fixture
 def client():
@@ -27,7 +27,7 @@ def pid():
                   size = CX.JUB_CLIENT_OBSERVATORY_ID_SIZE)
 
 def test_create_product(client,pid):
-    
+
     product =  Product(
             pid=pid,
         )
@@ -42,10 +42,54 @@ def test_get_products(client):
     assert isinstance(res.unwrap(),list)
 
 def test_query_products(client):
-    products = [
-        Product
-    ]
+    res_observatory = client.create_observatory(Observatory())
+    assert res_observatory.is_ok
+    obid = res_observatory.unwrap()
+    res_catalog = client.create_catalog(Catalog())
+    assert res_catalog.is_ok
+    cid = res_catalog.unwrap()
+    
+    res_update_observatory_catalogs = client.update_observatory_catalogs(obid,[
+        LevelCatalog(
+            level = 1,
+            cid = cid
+        )
+    ])
+    assert res_update_observatory_catalogs.is_ok
+    
+    p1_id = nanoid(alphabet=CX.JUB_CLIENT_OBSERVATORY_ID_ALPHABET,
+                  size = CX.JUB_CLIENT_OBSERVATORY_ID_SIZE)
+    
+    p2_id = nanoid(alphabet=CX.JUB_CLIENT_OBSERVATORY_ID_ALPHABET,
+                  size = CX.JUB_CLIENT_OBSERVATORY_ID_SIZE)
 
+    products = [
+        Product(
+            pid = p1_id,
+            cid = cid
+        ),
+        Product(
+            pid = p2_id,
+            cid = cid
+        )
+    ]
+    
+    res_create_products = client.create_products(products)
+    assert res_create_products.is_ok
+    
+    res_query = client.query_products(obid,ProductFilter())
+    assert res_query.is_ok
+
+    list_products = res_query.unwrap() 
+    assert isinstance(list_products,list)
+    assert len(list_products)>= 2
+    
+    
+    client.delete_product(p1_id)
+    client.delete_product(p2_id)
+    client.delete_observatory(obid)
+    client.delete_catalog(cid)
+    
 
 def test_delete_product(client,pid):
     res_create = client.create_products([
